690ee453d1fcd08fe88c8d0ef2734909ae75f4e2
zncs520 <70083721@qq.com>
2022-03-29 18:45:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,3 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/ssid=OpenWrt/ssid=KKK-NAS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
